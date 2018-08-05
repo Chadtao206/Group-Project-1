e3de5ea4e0d3af9c627dbccbbfc9f3e748168fe0
Chad Tao <chenchen408@gmail.com>
2018-08-04 21:27:46 -0700

    fixed a few things and added page flipper on search results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     var catID;
     var city;
     var citySearch = false;
+    var pages;
 
     //fontawesome icon array
     var iconArray = ["<i class='fas fa-truck'></i>", "<i class='far fa-calendar-alt'></i>", "<i class='far fa-moon'></i>", "<i class='far fa-clock'></i>", "<i class='fas fa-box'></i>", "<i class='fas fa-coffee'></i>", "<i class='fas fa-book-open'></i>", "<ion-icon name='sunny'></ion-icon>", "<ion-icon name='pizza'></ion-icon>", "<i class='fas fa-utensils'></i>", "<ion-icon name='beer'></ion-icon>", "<i class='far fa-money-bill-alt'></i>", "<i class='fas fa-cocktail'></i>"];
@@ -75,6 +76,8 @@ $(document).ready(function () {
             function searchCity() {
                 if (citySearch){
                     searchURL = "https://developers.zomato.com/api/v2.1/search?start=" + resultStart + "&count=" + resultShow + "&entity_id=" + cityID + "&entity_type=city&category=" + catID + "&sort=rating&order=desc";
+                }else{
+                    searchURL = "https://developers.zomato.com/api/v2.1/search?start=" + resultStart + "&count=" + resultShow + "&lat=" + searchLat + "&lon=" + searchLon + "&sort=rating&order=desc";
                 }
                 restaurant = [];
                 displayNum = 10;
@@ -84,6 +87,7 @@ $(document).ready(function () {
                     headers: { "user-key": apiKey },
                 }).then(function (response) {
                     console.log(response);
+                    pages = Math.ceil(response.results_found/10);
                     if (response.results_found < 11) {
                         displayNum = response.results_found;
                     }
@@ -111,8 +115,21 @@ $(document).ready(function () {
                 $("#map").show();
                 console.log(restaurant);
                 for (i = 0; i < restaurant.length; i++) {
-                    $(".searchresults").append("<div class='card' style='width: 12rem;'><img class='card-img-top' src='" + restaurant[i].thumbnail + "' alt='Card image cap'><div class='card-body'><h5 class='card-title'>" + restaurant[i].name + "</h5><p class='card-text' style='font-size:12px;'>" + restaurant[i].location.address + "</p><a target='_blank' href='" + restaurant[i].link + "' class='btn btn-primary' style='position:relative;bottom:10px;'>Zomato Page</a></div></div>");
+                    $(".searchresults").append("<div class='card' style='width: 12rem;'><img class='card-img-top' src='" + restaurant[i].thumbnail + "' alt='Card image cap'><div class='card-body'><h5 class='card-title' style='height:50px;overflow:hidden;'>" + restaurant[i].name + "</h5><p class='card-text' style='font-size:12px;height:60px;'>" + restaurant[i].location.address + "</p><a target='_blank' href='" + restaurant[i].link + "' class='btn btn-primary' style='margin-left:15px;'>Zomato Page</a></div></div>");
                 }
+                $(".nextbutton").html("<button class='btn btn-outline-primary btn-lg shadow-sm previous' style='margin:auto;margin-top:30px;'>Previous Page</button><h4 style='margin:auto;margin-top:60px;'>Page "+parseInt(resultStart/10+1)+" of "+pages+"</h4><button class='btn btn-outline-primary btn-lg shadow-sm next' style='margin:auto;margin-top:30px;'>Next Page</button>");
+                $(".next").on("click", function(){
+                    resultStart += 10;
+                    console.log("resultstart="+resultStart);
+                    searchCity();
+                })
+                $(".previous").on("click", function(){
+                    if (resultStart>=10){
+                      resultStart -= 10;
+                    console.log("resultstart="+resultStart);
+                    searchCity();  
+                    }
+                })
             }
 
 
@@ -124,6 +141,7 @@ $(document).ready(function () {
                     headers: { "user-key": apiKey },
                 }).then(function (cityResult) {
                     if (cityResult.location_suggestions.length > 1) {
+                        $(".nextbutton").empty();
                         $(".searchresults").html("<div style='margin-top:30px;color:black'><h1>Multiple locations found, did you mean -</h1></div>");
                         for (i = 0; i < cityResult.location_suggestions.length; i++) {
                             $(".searchresults").append("<div style='width:60%;margin:auto;' class='btn btn-secondary btn-lg multiple' cityID='" + cityResult.location_suggestions[i].id + "'>" + cityResult.location_suggestions[i].name + ", " + cityResult.location_suggestions[i].country_name + "?</div>")
