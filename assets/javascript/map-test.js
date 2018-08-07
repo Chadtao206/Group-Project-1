453a0a2aae39f7b12705213f6aef141b970cf294
vflores <valerie.flores64@gmail.com>
2018-08-06 20:19:29 -0700

    updated url for Google Maps API in index.html, moved Google marker comment out of map-test.js

--- assets/javascript/map-test.js
+++ assets/javascript/map-test.js
@@ -111,3 +111,9 @@ var bindInfoWindow = function (marker, map, infowindow, html) {
         infowindow.open(map, marker);
     });
 }
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+$("#myBtn").on("click", topFunction());
