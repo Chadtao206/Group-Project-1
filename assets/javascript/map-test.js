6ae8c68c2aaa86211af7f03fb8f1e557128eaea5
Hopp Willi <38337505+jhwillia7@users.noreply.github.com>
2018-08-07 12:38:48 -0700

    More Changes to HTML and StyleSheet

--- assets/javascript/map-test.js
+++ assets/javascript/map-test.js
@@ -112,6 +112,7 @@ var bindInfoWindow = function (marker, map, infowindow, html) {
     });
 }
 
+
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
